78abb99a5be415f8af1a098d0eb8823cf593f9b2
Lee Sangdae <mousesd@gmail.com>
2018-07-19 16:33:55 +0900

    JBK GagaoTalk inital commit

---
+++ JBK/GagaoTalk/app/src/main/java/jbk/homenet/net/gagaotalk/MainActivity.java
@@ -0,0 +1,13 @@
+package jbk.homenet.net.gagaotalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
