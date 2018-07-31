6ef8660d450d11b1971ae3f6196db8f96eb2861b
JinsupShin <dkdnlqkdnl@gmail.com>
2018-07-31 23:25:21 +0900

    # 1 회원가입 작업중...

--- SJS/GagaoTalk/app/src/main/java/sjs/homenet/net/gagaotalk/MainActivity.java
+++ SJS/GagaoTalk/app/src/main/java/sjs/homenet/net/gagaotalk/MainActivity.java
@@ -1,13 +1,31 @@
 package sjs.homenet.net.gagaotalk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button button = (Button)findViewById(R.id.email_create_account_button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), SginUpActivity.class);
+//            }
+//        });
+
     }
 }
---
+++ SJS/GagaoTalk/app/src/main/java/sjs/homenet/net/gagaotalk/SginUpActivity.java
@@ -0,0 +1,13 @@
+package sjs.homenet.net.gagaotalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SginUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sgin_up);
+    }
+}
