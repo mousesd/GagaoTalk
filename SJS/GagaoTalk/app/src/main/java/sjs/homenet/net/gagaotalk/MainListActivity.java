4dfc7b35f6f724c0315c7ca534cde9d86ba1769e
신진섭 <dkdnlqkdnl@gmail.com>
2018-09-06 19:31:15 +0900

    #2 친구목록(작업중)

--- SJS/GagaoTalk/app/src/main/java/sjs/homenet/net/gagaotalk/MainListActivity.java
+++ SJS/GagaoTalk/app/src/main/java/sjs/homenet/net/gagaotalk/MainListActivity.java
@@ -2,6 +2,7 @@ package sjs.homenet.net.gagaotalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainListActivity extends AppCompatActivity {
 
@@ -9,5 +10,26 @@ public class MainListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1);
+        tabHost1.setup();
+
+        // 첫번째 Tab
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
+        ts1.setContent(R.id.content1);
+        ts1.setIndicator("친구목록");
+        tabHost1.addTab(ts1);
+
+        // 두번째 Tab
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2");
+        ts2.setContent(R.id.content2);
+        ts2.setIndicator("대화목록");
+        tabHost1.addTab(ts2);
+
+        // 세번째 Tab
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3");
+        ts3.setContent(R.id.content3);
+        ts3.setIndicator("개인설정");
+        tabHost1.addTab(ts3);
     }
 }
